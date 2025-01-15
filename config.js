71af41ccb9fc1ab215fe6b0f9edcbd5fe980d67e
unknown <nguyenchauhuutan@gmail.com>
2025-01-15 16:44:37 +0700

    update

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const config = {
   mongoURI:
     "mongodb+srv://tranquangdung2809:gSz98HJvRf8ZBODV@cluster0.54v2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 };
-
+//update
 export default config;
