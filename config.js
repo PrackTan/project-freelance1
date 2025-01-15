d4395183539753f0b87e291d47059baab7ba9385
unknown <nguyenchauhuutan@gmail.com>
2025-01-15 17:42:04 +0700

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   mongoURI:
-    //"mongodb+srv://nguyenchauhuutan:ugTsjyHwTNKfVV7F@test.a2lsj.mongodb.net/?retryWrites=true&w=majority&appName=test",
-    "mongodb+srv://tranquangdung2809:Xd6Ysq1S6x15hJrF@cluster0.54v2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://nguyenchauhuutan:ugTsjyHwTNKfVV7F@test.a2lsj.mongodb.net/?retryWrites=true&w=majority&appName=test",
+  // "mongodb+srv://tranquangdung2809:Xd6Ysq1S6x15hJrF@cluster0.54v2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 };
 //update
 export default config;
